ac8d2d666ef058c5fc567068afd0fe9e7a19cbf1
Rohit <rohit055428@gmail.com>
2023-10-07 23:43:16 +0530

    07-10-2023

--- Recursion/TilingProblem.java
+++ Recursion/TilingProblem.java
@@ -1,28 +1,31 @@
 package Recursion;
 
 public class TilingProblem {
-    
-    public static int tilingProblem(int n ){
+
+    public static int tilingProblem(int n) {
         // base case
-        if(n==0 || n==1){
+        if (n == 0 || n == 1) {
             return 1;
         }
-
-        //kaam
+        return tilingProblem(n - 1) + tilingProblem(n - 2);
+        // kaam
         // vertical choice
-        int fnm1=tilingProblem(n-1);
+        /*
+         * int fnm1=tilingProblem(n-1);
+         * 
+         * // horizontal choice
+         * int fnm2=tilingProblem(n-2);
+         * 
+         * // total ways
+         * int totalWays = fnm1 + fnm2;
+         * 
+         * return totalWays;
+         */
 
-        // horizontal choice
-        int fnm2=tilingProblem(n-2);
-
-        // total ways
-        int totalWays = fnm1 + fnm2;
-
-        return totalWays;
     }
 
     public static void main(String[] args) {
-        System.out.println(tilingProblem(2));
+        System.out.println(tilingProblem(5));
     }
 
 }
